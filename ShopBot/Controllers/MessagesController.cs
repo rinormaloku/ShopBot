d6c04207db08b4f120302a89d7e9216dff531e28
rinormaloku <rinormaloku37@gmail.com>
2018-03-02 16:02:19 +0100

    * Added Bing SpellCheck API

--- ShopBot/Controllers/MessagesController.cs
+++ ShopBot/Controllers/MessagesController.cs
@@ -1,15 +1,22 @@
-﻿using System.Net;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Configuration;
 using System.Web.Http;
+using Microsoft.Azure.CognitiveServices.SpellCheck;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using ShopBot.Services;
 
 namespace ShopBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        private static readonly string ApiKey = WebConfigurationManager.AppSettings["BingSpellCheckApiKey"];
+        private readonly SpellCheckAPI _spellCheckApi = new SpellCheckAPI(new ApiKeyServiceClientCredentials(ApiKey));
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -18,6 +25,7 @@ namespace ShopBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
+                await FixMessageSpelling(activity);
                 await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
             }
             else
@@ -26,6 +34,18 @@ namespace ShopBot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private async Task FixMessageSpelling(Activity activity)
+        {
+            try
+            {
+                activity.Text = await _spellCheckApi.SpellCheck(activity.Text);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+            }
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ ShopBot/Services/SpellCheckExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.SpellCheck;
+using Microsoft.Azure.CognitiveServices.SpellCheck.Models;
+
+namespace ShopBot.Services
+{
+    public static class SpellCheckExt
+    {
+        public static async Task<string> SpellCheck(this SpellCheckAPI api, string spellcheck)
+        {
+            return (await api.SpellCheckerWithHttpMessagesAsync(text: spellcheck, mode: "proof"))
+                .Body
+                .FlaggedTokens
+                .Where(token => token.Suggestions.First().Score > 0.70)
+                .Select<SpellingFlaggedToken, Func<string, string>>(token => 
+                    (value => value.Replace(token.Token, token.Suggestions.First().Suggestion)))
+                .DefaultIfEmpty(_ => _)
+                .Aggregate((a, b) => s => a(b(s)))
+                .Invoke(spellcheck);
+        }
+    }
+}
